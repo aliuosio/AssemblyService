f75d7f62928399fad20a2a8fec327578e1b1bf4c
Osiozekhai Aliu <oaliu@hotmail.com>
2024-05-02 20:01:56 +0200

    fixed custom afixed assembly product custom options

--- Setup/Patch/Data/ProductAssemblyService.php
+++ Setup/Patch/Data/ProductAssemblyService.php
@@ -5,12 +5,13 @@ namespace BIWAC\AssemblyService\Setup\Patch\Data;
 use BIWAC\AssemblyService\Api\ConfigInterface;
 use Magento\Catalog\Api\Data\ProductCustomOptionInterface;
 use Magento\Catalog\Api\Data\ProductCustomOptionInterfaceFactory;
-use Magento\Catalog\Api\ProductCustomOptionRepositoryInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
+use Magento\Catalog\Api\ProductCustomOptionRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product\Attribute\Source\Status;
 use Magento\Catalog\Model\Product\Visibility;
+use Magento\CatalogInventory\Model\StockRegistry;
 use Magento\Framework\App\Area;
 use Magento\Framework\App\State;
 use Magento\Framework\Exception\CouldNotSaveException;
@@ -19,34 +20,41 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\StateException;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
-use Magento\CatalogInventory\Model\StockRegistry;
 
 class ProductAssemblyService implements DataPatchInterface
 {
-    private array $options  = [
-        [
-            'title' => 'Parent Product',
+    const OPTIONS = [
+        '0' => [
+            'sort_order' => '1',
+            'title' => 'Product to Assemble SKU',
+            'price_type' => 'fixed',
+            'price' => '0',
             'type' => 'field',
-            'is_require' => false,
-            'sort_order' => 0,
+            'is_require' => '1',
+            'is_hidden' => '1',
         ],
-        [
+        '1' => [
+            'sort_order' => '2',
             'title' => 'Product Class Price',
+            'price_type' => 'fixed',
+            'price' => '0',
             'type' => 'field',
-            'is_require' => false,
-            'sort_order' => 1,
+            'is_require' => '0',
+            'is_hidden' => '1',
         ]
     ];
 
     public function __construct(
-        readonly private ConfigInterface $config,
-        readonly private State $appState,
-        readonly private ProductInterfaceFactory $productFactory,
-        readonly private ProductRepositoryInterface $productRepository,
-        readonly private StockRegistry $stockRegistry,
-        readonly private ProductCustomOptionInterfaceFactory $customOptionFactory,
+        readonly private ConfigInterface                        $config,
+        readonly private State                                  $appState,
+        readonly private ProductInterfaceFactory                $productFactory,
+        readonly private ProductRepositoryInterface             $productRepository,
+        readonly private StockRegistry                          $stockRegistry,
+        readonly private ProductCustomOptionInterfaceFactory    $customOptionFactory,
         readonly private ProductCustomOptionRepositoryInterface $customOptionRepository
-    ) {}
+    )
+    {
+    }
 
     /**
      * @throws NoSuchEntityException
@@ -60,10 +68,8 @@ class ProductAssemblyService implements DataPatchInterface
         $this->appState->setAreaCode(Area::AREA_ADMINHTML);
 
         $product = $this->setProduct();
-        $this->productRepository->save($product);
         $this->setStock($product);
-
-        // $this->addCustomOptions($product);
+        $this->addCustomOptions($product);
     }
 
     public static function getDependencies(): array
@@ -76,6 +82,11 @@ class ProductAssemblyService implements DataPatchInterface
         return [];
     }
 
+    /**
+     * @throws CouldNotSaveException
+     * @throws StateException
+     * @throws InputException
+     */
     private function setProduct(): ProductInterface
     {
         $product = $this->productFactory->create();
@@ -86,6 +97,8 @@ class ProductAssemblyService implements DataPatchInterface
             ->setStatus(Status::STATUS_ENABLED)
             ->setVisibility(Visibility::VISIBILITY_BOTH)
             ->setTypeId('simple');
+
+        $this->productRepository->save($product);
 
         return $product;
     }
@@ -103,27 +116,16 @@ class ProductAssemblyService implements DataPatchInterface
         );
     }
 
-    /**
-     * @throws StateException
-     * @throws CouldNotSaveException
-     * @throws InputException
-     */
     private function addCustomOptions(ProductInterface $product): void
     {
-        foreach ($this->options as $arrayOption) {
-            $option = $this->getCustomOption()
-                ->setProductId($product->getId())
-                ->setProductSku($this->config->getSKU())
+        foreach (self::OPTIONS as $arrayOption) {
+            $option = $this->customOptionFactory->create();
+            $option->setProductId($product->getId())
+                ->setProductSku($product->getSku())
+                ->setStoreId($product->getStoreId())
                 ->addData($arrayOption);
             $this->customOptionRepository->save($option);
             $product->addOption($option);
         }
-
-        $this->productRepository->save($product);
-    }
-
-    private function getCustomOption(): ProductCustomOptionInterface
-    {
-        return $this->customOptionFactory->create();
     }
 }
