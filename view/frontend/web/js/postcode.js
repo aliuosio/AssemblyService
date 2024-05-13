7fe41db296d22121f0ab9091f3f7bdcb201a64b8
Osiozekhai Aliu <oaliu@hotmail.com>
2024-05-13 19:58:22 +0200

    set postcode as custom option

--- view/frontend/web/js/postcode.js
+++ view/frontend/web/js/postcode.js
@@ -1,98 +1,74 @@
-define([
-    'jquery'], function ($) {
+define(['jquery'], function ($) {
     'use strict';
 
-    var postcode = ''; // Declare postcode as a global variable
+    var form = $('#assembly-service'); // Cache form selection
+    var action = form.attr('action'); // Cache form action
+
+    function updatePrice() {
+        var class_id = $('#class-id').val();
+        var product_price = $('#current-product-price').val();
+        var price = 0;
+        var newPrice = 0;
+        var postcode = $('#postcode').val();
+
+        if (postcode.length >= 5) {
+            $.ajax({
+                url: '/postcodes/price/index',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    postcode: postcode,
+                    class_id: class_id,
+                },
+                success: function (response) {
+                    if (response.success) {
+                        price = parseFloat(response.price);
+                        var code = (price > 0) ? postcode : 0;
+                        updateFormAction(code);
+                    }
+                    newPrice = parseFloat(price) + parseFloat(product_price);
+                    $('.postcode-price').text('+ ' + price.toFixed(2));
+                    $('#postcode-price').val(price.toFixed(2));
+                    $('#price-boxer').text(newPrice.toFixed(2));
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', error);
+                }
+            });
+        } else {
+            $('.postcode-price').text('+ ' + price.toFixed(2));
+            if (product_price > 0)
+                $('#price-boxer').text(parseFloat(product_price).toFixed(2));
+        }
+    }
+
+    function updateFormAction(newValue) {
+        // Extract the original URL
+        var originalUrl = $("form#assembly-service").attr("action");
+
+        // Parse the URL
+        var urlParts = originalUrl.split('?');
+        var baseUrl = urlParts[0];
+        var queryParams = urlParts[1].split('&');
+
+        // Find the pre-last key-value pair
+        var preLastParam = queryParams[queryParams.length - 2];
+        var keyValue = preLastParam.split('=');
+        var key = keyValue[0];
+
+        // Modify the value with the new value
+        queryParams[queryParams.length - 2] = key + '=' + newValue;
+
+        // Reconstruct the URL
+        var modifiedUrl = baseUrl + '?' + queryParams.join('&');
+
+        // Update the form action attribute with the modified URL
+        $("form#assembly-service").attr("action", modifiedUrl);
+    }
 
     return function () {
         $('#postcode').on('input', function () {
-            postcode = $(this).val(); // Update the global postcode variable
             updatePrice();
-        })
-
-        function addToCustomOptions(price) {
-            var form = $('#assembly-service');
-            var action = form.attr('action');
-            var currentOptions = form.serializeArray();
-            var optionExists = false;
-
-            // Check if options[4] already exists, if so, replace its value
-            for (var i = 0; i < currentOptions.length; i++) {
-                if (currentOptions[i].name === 'options[4]') {
-                    currentOptions[i].value = postcode;
-                    optionExists = true;
-                    break;
-                }
-            }
-            console.log('TEST');
-            console.log(optionExists);
-
-            // If options[4] doesn't exist, add it to the form data
-            if (price > 0) {
-                if (!optionExists) {
-                    currentOptions.push({name: 'options[4]', value: postcode});
-                }
-
-                // Serialize the updated form data
-                var newDataSerialized = $.param(currentOptions);
-
-                // Set the action attribute with the updated form data
-                if (action.indexOf('?') !== -1) {
-                    form.attr('action', action.split('?')[0] + '?' + newDataSerialized);
-                } else {
-                    form.attr('action', action + '?' + newDataSerialized);
-                }
-            } else if (price === 0) {
-                // Remove the option value if the price is 0
-                var indexToRemove = currentOptions.findIndex(function (option) {
-                    return option.name === 'options[4]';
-                });
-                if (indexToRemove !== -1) {
-                    currentOptions.splice(indexToRemove, 1);
-                }
-            }
-
-            // Serialize the updated form data
-            var newDataSerialized = $.param(currentOptions);
-
-            // Set the action attribute with the updated form data
-            if (action.indexOf('?') !== -1) {
-                form.attr('action', action.split('?')[0] + '?' + newDataSerialized);
-            } else {
-                form.attr('action', action + '?' + newDataSerialized);
-            }
-        }
-
-        function updatePrice() {
-            var class_id = $('#option_3').val();
-            var price = 0;
-
-            if (postcode.length >= 5) { // Use the global postcode variable
-                $.ajax({
-                    url: '/postcodes/price/index',
-                    type: 'POST',
-                    dataType: 'json',
-                    data: {
-                        postcode: postcode, // Use the global postcode variable
-                        class_id: class_id,
-                    },
-                    success: function (response) {
-                        if (response.success) {
-                            price = parseFloat(response.price);
-                            addToCustomOptions(price);
-                        }
-
-                        $('.postcode-price').text(price.toFixed(2));
-                    },
-                    error: function (xhr, status, error) {
-                        console.error('Error:', error);
-                    }
-                });
-            } else {
-                $('.postcode-price').text(price.toFixed(2));
-            }
-        }
-
-        updatePrice();
+        });
     };
 });
